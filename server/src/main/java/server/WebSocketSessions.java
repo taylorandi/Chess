4376aa200c18e9115d1d0c7b529d1391b6e7a126
Taylor Anderton <taylorj.anderton@gmail.comm>
2024-04-01 21:20:29 -0600

    Routine check in at the end of the day, worked on creating gameplay client, still need to create websocket facade.

---
+++ server/src/main/java/server/WebSocketSessions.java
@@ -0,0 +1,38 @@
+package server;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.util.Map;
+
+;
+
+public class WebSocketSessions {
+    private Map<Integer, Map<String, Session>> sessions;
+
+    public void addSessionToGame(int gameID, String authToken, Session session){
+        Map<String, Session> current = null;
+        current.put(authToken, session);
+        sessions.put(gameID, current);
+    }
+
+    public void removeSession(int gameID, String authToken, Session session){
+        Map<String, Session> current;
+        current = sessions.get(gameID);
+        current.remove(authToken);
+    }
+
+    public void removeSession(Session session){
+        String auth = null;
+        for(Map<String, Session> current : sessions.values() ){
+            for (String key : current.keySet()){
+                if(current.get(key).equals(session)){
+                    auth = key;
+                }
+            }
+            current.remove(auth);
+
+        }
+    }
+
+
+}
